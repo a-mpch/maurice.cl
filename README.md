13cf75082df474004cdd79841e19aecc0934411f
Maurice Poirrier Chuden <mauricepoirrier@gmail.com>
2025-05-15 15:04:50 -0400

    feat: open new tab always

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Maurice.cl
 Home page of me? Just to tryout amp coding tool.
 
+## Run?
+`python -m http.server 8000`
+
 ## MCP server
 1. Run playwright npx @playwright/mcp@latest --port 8931
 2. Modify amp to server name playwright and url to `http://localhost:8931/sse`
--- components/sidebar.js
+++ components/sidebar.js
@@ -8,7 +8,7 @@ function createSidebar() {
         <nav>
             <ul>
                 <li><a href="about.html" class="${currentPage === 'about.html' ? 'active-page' : ''}">About</a></li>
-                <li><a href="https://world.hey.com/mpch" class="${currentPage === 'blog.html' ? 'active-page' : ''}">Blog</a></li>
+                <li><a href="https://world.hey.com/mpch" target="_blank" class="${currentPage === 'blog.html' ? 'active-page' : ''}">Blog</a></li>
                 <li><a href="talks.html" class="${currentPage === 'talks.html' ? 'active-page' : ''}">Talks</a></li>
                 <li><a href="misc.html" class="${currentPage === 'misc.html' ? 'active-page' : ''}">Misc</a></li>
                 <li><a href="contact.html" class="${currentPage === 'contact.html' ? 'active-page' : ''}">Contact</a></li>
